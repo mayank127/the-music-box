48238142339f58ce655e3f6a84d52a1794cae9f3
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2013-10-04 23:30:33 +0530

    changinh orientation of hand

--- body.cpp
+++ body.cpp
@@ -13,14 +13,14 @@ void body::init(){
 	thighR = bone(0.0, -4.0, 0.0,"thighR");
 	legL = bone(0.0, -5.0, 0.0,"legL");
 	legR = bone(0.0, -5.0, 0.0,"legR");
-	footL = bone(1.0, 0.0, 1.0,"footL");
-	footR = bone(1.0, 0.0, 1.0,"footR");
+	footL = bone(0.0, -1.0, 0.0,"footL");
+	footR = bone(0.0, -1.0, 0.0,"footR");
 	uarmL = bone(0.0, -3.0, 0.0,"uarmL");
 	uarmR = bone(0.0, -3.0, 0.0,"uarmR");
 	larmL = bone(0.0, -3.0, 0.0,"larmL");
 	larmR = bone(0.0, -3.0, 0.0,"larmR");
-	handL = bone(1.0, 0.0, 1.0,"handL");
-	handR = bone(1.0, 0.0, 1.0,"handR");
+	handL = bone(0.0, -1.0, 0.0,"handL");
+	handR = bone(0.0, -1.0, 0.0,"handR");
 
 	torso2 = joint(0.0, 5.0, 0.0, 0.0, 0.0, 0.0, &torso1,"torso2");
 	neck = joint(0.0, 5.0, 0.0, 0.0, 0.0, 0.0, &head,"neck");
@@ -51,23 +51,6 @@ void body::init(){
 	uarmR.childeren.push_back(&elbowR);
 	larmL.childeren.push_back(&wristL);
 	larmR.childeren.push_back(&wristR);
-
-
-	torso3.name = "torso3";
-	torso1.name = "torso1";
-	head.name = "head";
-	thighL.name = "thighL";
-	thighR.name = "thighR";
-	legL.name = "legL";
-	legR.name = "legR";
-	footL.name = "footL";
-	footR.name = "footR";
-	uarmL.name = "uarmL";
-	uarmR.name = "uarmR";
-	larmL.name = "larmL";
-	larmR.name = "larmR";
-	handL.name = "handL";
-	handR.name = "handR";
 }
 body::body(){
 	this->x = 0;
