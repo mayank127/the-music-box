8d7728c8056f0e1301aa4953203793d05c2accd0
Aditya Raj <adityarajrao@gmail.com>
2013-10-06 00:05:44 +0530

    display list made for box

--- box.cpp
+++ box.cpp
@@ -8,36 +8,53 @@ box::box(float size, float rx, float x, float y, float z){
 	this->x= x;
 	this->y= y;
 	this->z= z;
+	createDL();
 }
 
 box::box(){
 	this->size = 10;
-	this->rx = 90-30;
+	this->rx = 90;
 	this->x= 0;
 	this->y= 0;
 	this->z= 0;
+	createDL();
 }
 
-void box::rectangle(){
-	glBegin(GL_QUADS);
+
+void box::createDL(){
+  
+  
+  // Create the id for the list
+  boxDL = glGenLists(1);
+  
+  // start list
+  glNewList(boxDL,GL_COMPILE);
+  
+  // call the function that contains the rendering commands
+  glBegin(GL_QUADS);
 		glVertex3f(0.0, 0.0, 0.0);
 		glVertex3f(size, 0.0, 0.0);
 		glVertex3f(size, size, 0.0);
 		glVertex3f(0.0, size, 0.0);
 	glEnd();
+  
+  // endList
+  glEndList();
+  
 }
+
 
 void box::draw(){
 	glPushMatrix();
 		glTranslatef(x,y,z);
 		//front
 		glColor3f(1.0, 1.0, 1.0);
-		rectangle();
+		glCallList(boxDL);
 		//below
 		glPushMatrix();
 			glRotatef(-90, 1.0, 0.0, 0.0);
 			glColor3f(1.0, 0.0, 0.0);
-			rectangle();
+			glCallList(boxDL);
 		glPopMatrix();
 		//top
 		glPushMatrix();
@@ -47,26 +64,26 @@ void box::draw(){
 			else if(rx<0) rx = 0;
 			glRotatef(rx, 1.0, 0.0, 0.0);
 			glColor3f(1.0, 1.0, 0.0);
-			rectangle();
+			glCallList(boxDL);
 		glPopMatrix();
 		//left
 		glPushMatrix();
 			glRotatef(90, 0.0, 1.0, 0.0);
 			glColor3f(0.0, 1.0, 0.0);
-			rectangle();
+			glCallList(boxDL);
 		glPopMatrix();
 		//right
 		glPushMatrix();
 			glTranslatef(size, 0.0, 0.0);
 			glRotatef(90, 0.0, 1.0, 0.0);
 			glColor3f(0.0, 1.0, 0.0);
-			rectangle();
+			glCallList(boxDL);
 		glPopMatrix();
 		//back
 		glPushMatrix();
 			glColor3f(0.0, 0.0, 1.0);
 			glTranslatef( 0.0, 0.0, -size);
-			rectangle();
+			glCallList(boxDL);
 		glPopMatrix();
 	glPopMatrix();
 }
--- box.h
+++ box.h
@@ -8,13 +8,14 @@ using namespace std;
 //Class for box object
 class box {
 	public:
+		GLuint boxDL;
 		float size;
 		float rx,x,y,z;
 		//constructor for box
 		box(float,float,float,float,float);	
 		box();											//null constructor
 
-		void rectangle();
+		void createDL();
 		void draw();		//draw function
 };
 #endif
